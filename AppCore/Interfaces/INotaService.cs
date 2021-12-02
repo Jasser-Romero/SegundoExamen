904728f9bc44dfb79f8e993fc4fe50cc84c38b4d
JasserRomero247 <jasseromero08@gmail.com>
2021-12-02 08:48:13 -0600

    qwd

---
+++ AppCore/Interfaces/INotaService.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppCore.Interfaces
+{
+    public interface INotaService : IService<Nota>
+    {
+        int GetLastId();
+        decimal Promedio(List<Nota> notas);
+        List<Nota> GetNotasById(int id);
+        ICollection<Nota> MejoresTres();
+    }
+}
