984fd24ea018c67295c0bcf3ea94caf400d0b13a
KevinMen7 <kevingfx71@gmail.com>
2021-12-02 08:21:41 -0600

    Subiendo

---
+++ Domain/Entities/Estudiante.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Entities
+{
+    public class Estudiante
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellidos {get; set;}
+        public string Departamento { get; set; }
+        public string Municipio { get; set; }
+        public decimal Promedio { get; set; }
+    }
+}
